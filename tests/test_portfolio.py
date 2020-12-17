5a2f589edb3118002a32586750fb8a06c909b2ea
Izzudin Hafiz <izzudin.hafiz@gmail.com>
2020-12-18 04:05:09 +0800

    Added short position testing for Portfolio class

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -21,7 +21,7 @@ API_KEY = os.getenv("API_KEY")
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url="https://paper-api.alpaca.markets")
 
 
-def test_portfolio_position():
+def test_portfolio_long_position():
     portfolio_label_data = []
     with open("tests\\portfolio_test.csv", "r", newline="") as f:
         reader = csv.reader(f)
@@ -44,3 +44,35 @@ def test_portfolio_position():
         assert Money(label_nett_gain) == portfolio.nett_gain
         assert Money(label_total_equity) == portfolio.cash + portfolio.equity
         mt.next_tick()
+
+
+def test_portfolio_short_position():
+    portfolio_label_data = []
+    with open("tests\\portfolio_short_test.csv", "r", newline="") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            portfolio_label_data.append(row)
+
+    mt = Market(asset_context=["MMM"], start_date=datetime(2020, 12, 1), end_date=datetime(2020, 12, 3))
+    portfolio = mt.register_portfolio(100_000)
+    portfolio.open_position_by_size("MMM", -500)
+
+    portfolio_label_data = portfolio_label_data[1:]
+
+    for time, label_price, label_value, label_gain, label_total_equity, label_nett_gain, label_margin in portfolio_label_data:
+        portfolio.update()
+        position = portfolio.open_positions[0]
+
+        if Money(label_price) != position.current_price:
+            print(mt.time_now, Money(label_price), position.current_price)
+
+        assert Money(label_price) == position.current_price
+        assert Money(label_value) == position.current_value
+        assert Money(label_gain) == position.gain
+        assert Money(label_nett_gain) == portfolio.nett_gain
+        assert Money(label_total_equity) == portfolio.cash + portfolio.equity
+        assert Money(label_margin) == portfolio.margin
+        mt.next_tick()
+
+
+# test_portfolio_short_position()
