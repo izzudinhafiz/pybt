9f5c1be4bc218dab67d825a08d19549a73d686c1
Izzudin Hafiz <izzudin.hafiz@gmail.com>
2020-12-17 23:28:02 +0800

    Fixed Position for opening/closing positions

--- commons/backtest/position.py
+++ commons/backtest/position.py
@@ -32,7 +32,7 @@ class Position:
         self.take_profit = None
         self.stop_loss = None
 
-        self.nett_gain = None
+        self.gain = None
         self.total_commission = self.open_commission + self.close_commission
 
     @classmethod
@@ -45,11 +45,17 @@ class Position:
         if current_price is None:
             return None
         size = value / current_price
-        position.open_position(symbol, size, current_price, take_profit, stop_loss)
+        is_open = position.open_position(symbol, size, current_price, take_profit, stop_loss)
 
-        return position
+        if is_open:
+            return position
+        else:
+            return None
 
     def open_position(self, symbol: str, size: Money, open_price: Money, take_profit=None, stop_loss=None):
+        if size == 0:
+            return None
+
         self.symbol = symbol
         self.size = size
         self.open_price = Money(open_price)
@@ -58,25 +64,25 @@ class Position:
         self.open_time = self.portfolio.market.time_now
         self.active = True
         self.position_type = "long" if size > 0 else "short"
-        self.open_commission = Money(self.open_value * self.commission_rate)
+        self.open_commission = Money(self.open_value * self.commission_rate).abs()
         self.total_commission = self.open_commission
-        self.current_value = self.current_price * self.size - self.total_commission
-        self.nett_gain = self.current_value - self.open_value
+        self.current_value = self.current_price * self.size
+        self.gain = self.current_value - self.open_value
 
         self.take_profit = take_profit
         self.stop_loss = stop_loss
 
-        return self.nett_gain
+        return self
 
     def close_position(self, close_type=None):
         self.close_time = self.portfolio.market.time_now
         self.close_price = self.portfolio.market.get_current_price(self.symbol)
         self.current_price = self.close_price if self.close_price else self.current_price  # Handles incomplete_price scenario
-        self.close_commission = Money(self.commission_rate * self.close_price * self.size)
+        self.current_value = self.current_price * self.size
+        self.close_commission = Money(self.commission_rate * self.current_value).abs()
         self.total_commission = self.open_commission + self.close_commission
-        self.current_value = self.current_price * self.size - self.total_commission
         self.close_value = self.current_value
-        self.nett_gain = self.current_value - self.open_value
+        self.gain = self.current_value - self.open_value
         self.active = False
 
         if close_type is None:
@@ -84,7 +90,7 @@ class Position:
         else:
             self.close_type = close_type
 
-        return self.nett_gain
+        return self
 
     def update(self):
         if not self.active:
@@ -100,10 +106,10 @@ class Position:
         if should_close:
             return self.close_position(should_close)
 
-        self.current_value = self.current_price * self.size - self.total_commission
-        self.nett_gain = self.current_value - self.open_value
+        self.current_value = self.current_price * self.size
+        self.gain = self.current_value - self.open_value
 
-        return self.nett_gain
+        return self.gain
 
     def terminate_early(self):
         warnings.warn(f"{self.symbol} Opened on: {self.open_time} Terminated on {self.portfolio.market.time_now} due to insufficient price data")
