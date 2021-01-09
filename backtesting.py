86dcd3146d4960d231ff16c63c1b7db01c71223f
Izzudin Hafiz <izzudin.hafiz@gmail.com>
2021-01-10 02:53:40 +0800

    Added sample Optimizer, Scorer and Indicator class

--- backtesting.py
+++ backtesting.py
@@ -1,32 +1,90 @@
-from commons.models.market_model import Asset, Price, Financial, pg_db, sq_db
+from commons.models.market_model import Asset, Price, Financial, pg_db
 from commons.backtest import Market
-from datetime import datetime, timedelta
-import alpaca_trade_api as tradeapi
-import os
-import time
-from pprint import pprint
-import random
+from datetime import datetime
+from commons.money import Money
+import talib
+import numpy as np
 pg_db.bind([Asset, Price, Financial])
-SECRET_KEY = os.getenv("SECRET_KEY")
-API_KEY = os.getenv("API_KEY")
-api = tradeapi.REST(API_KEY, SECRET_KEY, base_url="https://paper-api.alpaca.markets")
-
 
 assets_symbol = [x.symbol for x in Asset.select().where((Asset.sp500 == True) & (Asset.tradable == True))]
-mt = Market(asset_context=assets_symbol, end_date=datetime(2015, 1, 30))
-portfolio = mt.register_portfolio(1000*1000)
+mt = Market(asset_context=assets_symbol, start_date=datetime(2020, 1, 1), end_date=datetime(2020, 1, 31), debug_mode=1)
 
 
-for i in range(100):
-    portfolio.open_position_by_value(assets_symbol[i], 1000)
+class Indicator:
+    def __init__(self, symbol, duration=None):
+        self.symbol = symbol
+        self.duration = duration
 
-start_time = time.time()
 
-LOOP_COUNT = 100
-for i in range(LOOP_COUNT):
-    for position in portfolio.positions:
-        position.update()
-    mt.next_tick()
+class MAIndicator(Indicator):
+    def __init__(self, symbol, duration):
+        super().__init__(symbol, duration)
+        self.data = []
 
-elapsed = time.time() - start_time
-print(f"Total time: {elapsed:.2f} s | Avg Time/Loop: {(elapsed / LOOP_COUNT) *1000:.2f} ms | Avg Time/Loop/Position: {(elapsed / LOOP_COUNT / len(portfolio.positions)) * 1000 * 1000:.0f} µs ")
+    def update(self, current_price):
+        if len(self.data) >= self.duration:
+            self.data.pop(0)
+        self.data.append(current_price.cents/100)
+
+        if len(self.data) == self.duration:
+            return Money(talib.SMA(np.asarray(self.data))[-1])
+        else:
+            return None
+
+
+class Scorer:
+
+    def __init__(self, portfolio, market):
+        self.portfolio = portfolio
+        self.market = market
+        self.indicators = []
+        self.indicators.append(MAIndicator("AAPL", 30))
+        self.indicators.append(MAIndicator("GOOGL", 30))
+
+    def execute(self):
+        '''
+        Do some arbitary calculation on stock metrics and return scores of all possible stock in the market
+        '''
+        data = {}
+
+        for indicator in self.indicators:
+            cur_price = self.market.get_current_price(indicator.symbol)
+            average = indicator.update(cur_price)
+            if average is None:
+                continue
+
+            price_delta = cur_price - average
+            price_change = price_delta / average
+
+            if price_change > 0.01:
+                data[indicator.symbol] = -1
+            elif price_change < -0.01:
+                data[indicator.symbol] = 1
+
+        return data
+
+
+class Optimizer:
+
+    def __init__(self, portfolio):
+        self.portfolio = portfolio
+
+    def execute(self, data):
+        '''
+        Analyze scores and current portfolio positions and return decision to be made
+        '''
+
+        for symbol, decision in data.items():
+            pos_type = "long" if decision == 1 else "short"
+            open_positions = self.portfolio.get_positions(symbol)
+            if open_positions:
+                for position in open_positions:
+                    if position.position_type != pos_type:
+                        position.close_position()
+            else:
+                self.portfolio.open_position_by_value(symbol, 1000)
+
+
+mt.register_portfolio(100_000, scorer=Scorer, optimizer=Optimizer)
+
+mt.run_simulation()
