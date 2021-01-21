463def6049f333f68ea49adaf83404d382358ef4
Izzudin Hafiz <izzudin.hafiz@gmail.com>
2021-01-22 02:09:10 +0800

    added some more helper functions. @timer wrapper and a deep get_size func

--- commons/helper.py
+++ commons/helper.py
@@ -1,4 +1,7 @@
 import inspect
+from functools import wraps
+from time import perf_counter
+import sys
 
 
 def get_caller(frame_stack):
@@ -28,3 +31,36 @@ def set_object_by_caller(caller_type):
             return caller
 
     return None
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = perf_counter()
+        value = func(*args, **kwargs)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        print(f"{func.__name__!r}: {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
